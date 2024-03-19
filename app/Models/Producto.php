9eec27d722a287e631b1ab6dcffd7afcb5a03433
smithacevedo <81036708+smithacevedo@users.noreply.github.com>
2024-03-18 20:31:31 -0500

    Primer Commit

---
+++ app/Models/Producto.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Producto extends Model
+{
+    use HasFactory;
+
+    // Instancio la tabla 'productos'
+    protected $table = 'productos';
+
+    // Declaro los campos que usaré de la tabla 'productos'
+    protected $fillable = ['nombre', 'precio', 'stock', 'img'];
+}
