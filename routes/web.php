0e8cd95776900682731ea49860722a8fb82c6cdb
smithacevedo <81036708+smithacevedo@users.noreply.github.com>
2024-03-19 01:39:10 -0500

    Se actualiza menú

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BiblioTecController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LibroController;
 
@@ -18,5 +19,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::resource('layouts', BiblioTecController::class);
 Route::resource('libro',LibroController::class);
+
 
