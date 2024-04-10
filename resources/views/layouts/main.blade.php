a093b81c6d5f93d5bb8e8db1f443f053f34bac72
santiago vela <santiago.vela@osiriscloud.co>
2024-04-09 21:05:13 -0500

    Revert "Update main.blade.php"
    This reverts commit 3cb8ab3819a4029d130c2b0c44937988117413ca.

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -130,11 +130,6 @@
             </li>
 
           </ul>
-          <div class="card-body">
-                          <div class="mb-2 mb-md-0  text-sm-right">
-                            <a href="{{ route('login.destroy') }}" class="btn btn-danger border">Cerrar sesión</a>
-                          </div>
-                        </div>
         </aside>
         <!-- / Menu -->
 
@@ -168,6 +163,11 @@
                             data-app-dark-img="illustrations/man-with-laptop-dark.png"
                             data-app-light-img="illustrations/man-with-laptop-light.png"
                           />
+                        </div>
+                        <div class="card-body">
+                          <div class="mb-2 mb-md-0  text-center text-sm-right">
+                            <a href="{{ route('login.destroy') }}" class="btn btn-danger border">Cerrar sesión</a>
+                          </div>
                         </div>
                       </div>
                     </div>
